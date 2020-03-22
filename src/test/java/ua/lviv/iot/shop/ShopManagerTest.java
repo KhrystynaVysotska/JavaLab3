4165478500a4de302e36606df425e570a60053a9
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-03-22 13:28:46 +0200

    Fixed bugs noticed by checkstyle

--- src/test/java/ua/lviv/iot/shop/ShopManagerTest.java
+++ src/test/java/ua/lviv/iot/shop/ShopManagerTest.java
@@ -12,25 +12,25 @@ import ua.lviv.iot.shop.model.Gender;
 import ua.lviv.iot.shop.model.Season;
 import ua.lviv.iot.shop.shopmanager.ShopManager;
 
-class ShopManagerTest extends BaseShopManagerTest{
+class ShopManagerTest extends BaseShopManagerTest {
 
-	private ShopManager shopManager;
-	
-	@BeforeEach
-	public void setUp(){
-		shopManager = new ShopManager();
-		shopManager.addGoods(sweaters);
-	}
-	
-	@Test
-	public void testFindGoodsByGender(){
-		List<AbstractGoodsForKids> foundedGoods = shopManager.findGoodsBy(Gender.GIRL);
-		assertEquals(2, foundedGoods.size());
-	}
-	
-	@Test
-	public void testFindGoodsBySeason() {
-		List<AbstractGoodsForKids> foundedGoods = shopManager.findGoodsBy(Season.AUTUMN);
-		assertEquals(3, foundedGoods.size());
-	}
+    private ShopManager shopManager;
+
+    @BeforeEach
+    public void setUp() {
+        shopManager = new ShopManager();
+        shopManager.addGoods(sweaters);
+    }
+
+    @Test
+    public void testFindGoodsByGender() {
+        List<AbstractGoodsForKids> foundedGoods = shopManager.findGoodsBy(Gender.GIRL);
+        assertEquals(2, foundedGoods.size());
+    }
+
+    @Test
+    public void testFindGoodsBySeason() {
+        List<AbstractGoodsForKids> foundedGoods = shopManager.findGoodsBy(Season.AUTUMN);
+        assertEquals(3, foundedGoods.size());
+    }
 }
