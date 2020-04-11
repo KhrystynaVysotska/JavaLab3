856fc089c3b67136ef1d9b363dfa172165feeb40
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-11 22:17:46 +0300

    Added @Repository annotation

--- src/main/java/ua/lviv/iot/dataaccess/SweaterRepository.java
+++ src/main/java/ua/lviv/iot/dataaccess/SweaterRepository.java
@@ -1,8 +1,10 @@
 package ua.lviv.iot.dataaccess;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.lviv.iot.shop.model.Sweater;
 
+@Repository
 public interface SweaterRepository extends JpaRepository<Sweater, Integer> {
 
 }
